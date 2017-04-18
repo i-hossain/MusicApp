04c2e402744292f88baf2359b3174ae6cbcb73d2
connected-ihossain <ihossain@connectedlab.com>
2017-04-18 18:32:11 -0400

    Can download images from URL's and set as Preview

--- MusicApp/MainVC.swift
+++ MusicApp/MainVC.swift
@@ -20,11 +20,11 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
         let urlTest = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/KQ6zr6kCPj8\" frameborder=\"0\" allowfullscreen></iframe>"
         
-        let p1 = PartyRock(imageURL: "http://www.wavefm.com.au/images/stories/2015/04/redfoo.jpg", videoURL: urlTest, videoTitle: "Party Rock Anthem")
-        let p2 = PartyRock(imageURL: "http://www.wavefm.com.au/images/stories/2015/04/redfoo.jpg", videoURL: urlTest, videoTitle: "Light's Out")
-        let p3 = PartyRock(imageURL: "http://www.wavefm.com.au/images/stories/2015/04/redfoo.jpg", videoURL: urlTest, videoTitle: "Let's Get Ridiculous")
-        let p4 = PartyRock(imageURL: "http://www.wavefm.com.au/images/stories/2015/04/redfoo.jpg", videoURL: urlTest, videoTitle: "Juicy Wiggle")
-        let p5 = PartyRock(imageURL: "http://www.wavefm.com.au/images/stories/2015/04/redfoo.jpg", videoURL: urlTest, videoTitle: "Juicy Wiggle Lesson")
+        let p1 = PartyRock(imageURL: "https://img.youtube.com/vi/SkTt9k4Y-a8/hqdefault.jpg", videoURL: urlTest, videoTitle: "Party Rock Anthem")
+        let p2 = PartyRock(imageURL: "https://img.youtube.com/vi/3BpdKCzHbq8/hqdefault.jpg", videoURL: urlTest, videoTitle: "Light's Out")
+        let p3 = PartyRock(imageURL: "https://i.ytimg.com/vi/2wUxw6GH3IM/hqdefault.jpg", videoURL: urlTest, videoTitle: "Let's Get Ridiculous")
+        let p4 = PartyRock(imageURL: "https://img.youtube.com/vi/tWyuglGCKgE/hqdefault.jpg", videoURL: urlTest, videoTitle: "Juicy Wiggle")
+        let p5 = PartyRock(imageURL: "https://img.youtube.com/vi/vwO5Di22QIg/hqdefault.jpg", videoURL: urlTest, videoTitle: "Juicy Wiggle Lesson")
         
         partyRocks.append(p1)
         partyRocks.append(p2)
