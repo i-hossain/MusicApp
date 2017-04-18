05c963b663de5efbdd90f7680ba7b636b739e654
connected-ihossain <ihossain@connectedlab.com>
2017-04-18 17:30:22 -0400

    Table view with 5 cells

--- MusicApp/MainVC.swift
+++ MusicApp/MainVC.swift
@@ -21,8 +21,16 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         let urlTest = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/KQ6zr6kCPj8\" frameborder=\"0\" allowfullscreen></iframe>"
         
         let p1 = PartyRock(imageURL: "http://www.wavefm.com.au/images/stories/2015/04/redfoo.jpg", videoURL: urlTest, videoTitle: "Party Rock Anthem")
+        let p2 = PartyRock(imageURL: "http://www.wavefm.com.au/images/stories/2015/04/redfoo.jpg", videoURL: urlTest, videoTitle: "Light's Out")
+        let p3 = PartyRock(imageURL: "http://www.wavefm.com.au/images/stories/2015/04/redfoo.jpg", videoURL: urlTest, videoTitle: "Let's Get Ridiculous")
+        let p4 = PartyRock(imageURL: "http://www.wavefm.com.au/images/stories/2015/04/redfoo.jpg", videoURL: urlTest, videoTitle: "Juicy Wiggle")
+        let p5 = PartyRock(imageURL: "http://www.wavefm.com.au/images/stories/2015/04/redfoo.jpg", videoURL: urlTest, videoTitle: "Juicy Wiggle Lesson")
         
         partyRocks.append(p1)
+        partyRocks.append(p2)
+        partyRocks.append(p3)
+        partyRocks.append(p4)
+        partyRocks.append(p5)
         
         tableView.delegate = self
         tableView.dataSource = self
