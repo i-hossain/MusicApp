9589aa5beeb2c19082c8ffd3db2727b8607325b5
connected-ihossain <ihossain@connectedlab.com>
2017-04-17 20:26:06 -0400

    Added UI Layouts

---
+++ MusicApp/MainVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Ismail Hossain on 2017-04-17.
+//  Copyright © 2017 Ismail Hossain. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
